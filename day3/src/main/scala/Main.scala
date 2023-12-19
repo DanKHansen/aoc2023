5006e9acff10a36640dad97444f0d893ab6449ce
dan.kjeldstrom.hansen <dan.kjeldstrom.hansen@qliro.com>
2023-12-19 13:43:18 +0100

    Solution to part 1

--- day3/src/main/scala/Main.scala
+++ day3/src/main/scala/Main.scala
@@ -1,13 +1,49 @@
 import scala.util.matching.Regex
-
 @main def day3: Unit =
-    input.foreach(println)
+    println(s)
 
-    def input: Iterator[String] =
-        io.Source
-            .fromFile("/home/dankh/scalaprojects/aoc_2023/sources/3a_ex.txt")
-            .getLines()
+def ls =
+    io.Source
+        .fromFile("/home/dankh/scalaprojects/aoc_2023/sources/3.txt")
+        .getLines()
+        .toArray
 
-    val regEx = """\d+""".r
+val regEx = """\d+""".r
 
-    input.map(l => regEx.findAllMatchIn(l).foreach(println))
+val r: Array[Iterator[String]] =
+    for l <- ls
+    yield {
+        for {
+            m <- regEx.findAllIn(l).matchData
+        } yield {
+            {
+                if (ls.indexOf(l) - 1 <= 0) ""
+                else
+                    ls(ls.indexOf(l) - 1).substring(
+                      if (m.start < 1) m.start else m.start - 1,
+                      if (m.end >= l.length) m.end else m.end + 1
+                    )
+            } + {
+                ls(ls.indexOf(l)).substring(
+                  if (m.start < 1) m.start else m.start - 1,
+                  if (m.end >= l.length) m.end else m.end + 1
+                )
+            } + {
+                if (ls.indexOf(l) + 1 >= ls.length - 1) ""
+                else
+                    ls(ls.indexOf(l) + 1).substring(
+                      if (m.start < 1) m.start else m.start - 1,
+                      if (m.end >= l.length) m.end else m.end + 1
+                    )
+            }
+        }
+    }
+
+val s: Int = r
+    .map(i =>
+        i.map(s =>
+            if (s.filter(_.!=('.')).filterNot(_.isDigit).length == 0) 0
+            else regEx.findFirstMatchIn(s).get.matched.toInt
+        ).sum
+    )
+    .sum
