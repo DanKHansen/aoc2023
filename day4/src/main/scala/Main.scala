6b747607ceb55fc91c1e9e28a03dbcbbf06f69ea
dan.kjeldstrom.hansen <dan.kjeldstrom.hansen@qliro.com>
2023-12-28 10:52:28 +0100

    Solution to 4a

--- day4/src/main/scala/Main.scala
+++ day4/src/main/scala/Main.scala
@@ -1,4 +1,30 @@
 @main def day4(): Unit =
-  println(day4a)
+    println(day4a)
 
-def day4a: Int = ???
+def day4a =
+    val ls: Iterator[String] = io.Source
+        .fromFile("/home/dankh/scalaprojects/aoc_2023/sources/4.txt")
+        .getLines
+
+    ls.map(l =>
+        (
+          l.substring(0, l.indexOf(":") + 1),
+          l.substring(l.indexOf(":") + 1, l.indexOf("|"))
+              .split(" ")
+              .map(_.trim)
+              .filterNot(_.isEmpty)
+              .map(_.toInt)
+              .toList,
+          l.substring(l.indexOf("|") + 1)
+              .split(" ")
+              .map(_.trim)
+              .filterNot(_.isEmpty)
+              .map(_.toInt)
+              .toList
+        )
+    ).map(winners => winners._2.length - (winners._2 diff winners._3).length)
+        .map(power => {
+            if power == 0 then 0 else scala.math.pow(2, power - 1)
+        })
+        .sum
+    // )
