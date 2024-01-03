d48bba6db49b62f9f60ba90a36a9b14d7914d91e
dan.kjeldstrom.hansen <dan.kjeldstrom.hansen@qliro.com>
2024-01-03 13:22:58 +0100

    4b with iteration and foldLeft

--- day4/src/main/scala/Main.scala
+++ day4/src/main/scala/Main.scala
@@ -1,32 +1,8 @@
-import scala.annotation.tailrec
-val src: List[String] = io.Source
-    .fromFile("/home/dankh/scalaprojects/aoc_2023/sources/4a_ex.txt")
-    .getLines
-    .toList
-@main def day4(): Unit =
-    println(day4b(src))
-
-def day4a(sc: Iterator[String]): Int =
-    sc.map(l =>
-        (
-          l.substring(l.indexOf(":") + 1, l.indexOf("|"))
-              .split(" ")
-              .map(_.trim)
-              .filterNot(_.isEmpty),
-          l.substring(l.indexOf("|") + 1)
-              .split(" ")
-              .map(_.trim)
-              .filterNot(_.isEmpty)
-        )
-    ).map(t => t._1.length - (t._1 diff t._2).length)
-        .map(p => if p == 0 then 0 else math.pow(2, p - 1).toInt)
-        .sum
-
-def day4b(s: List[String]): Int =
-    cards(winnerMap(s), winnerMap(s).keySet.toList, 1)
-
-private def winnerMap(sc: List[String]) =
-    val winners = sc
+lazy val wins: List[Int] =
+    io.Source
+        .fromFile("/home/dankh/scalaprojects/aoc_2023/sources/4.txt")
+        .getLines
+        .toList
         .map(l =>
             (
               l.substring(l.indexOf(":") + 1, l.indexOf("|"))
@@ -39,20 +15,21 @@ private def winnerMap(sc: List[String]) =
                   .filterNot(_.isEmpty)
             )
         )
-        .map(t => t._1.length - (t._1 diff t._2).length)
-    val origCard = Range.inclusive(1, winners.length).toList
-    origCard.zip(winners).toMap
+        .map(t => t._2.intersect(t._1).length)
+@main def runday4(): Unit =
+    println(day4a())
+    println(day4b())
 
-@tailrec
-def cards(m: Map[Int, Int], l: List[Int], count: Int): Int =
-    if (l.tail.isEmpty) count
-    else
-        cards(
-          m,
-          l.tail ++ Range.inclusive(
-            l.head + 1,
-            if (l.head + m(l.head) >= m.size) m.size
-            else l.head + m(l.head)
-          ),
-          count + 1
-        )
+def day4a(): Int =
+    wins.map(x => if x == 0 then 0 else math.pow(2, x - 1).toInt).sum
+
+def day4b(): Int =
+    val queue = collection.mutable.Queue(1)
+    wins.foldLeft(0) { (sum, score) =>
+        val count = if (queue.isEmpty) 1 else queue.dequeue()
+        for (i <- 0 until score) {
+            if (i < queue.size) queue.update(i, queue(i) + count)
+            else queue.append(count + 1)
+        }
+        sum + count
+    }
