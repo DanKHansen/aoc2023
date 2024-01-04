6ffdd82fcdd5f8ea5fcc523c495c9fcef881a3b0
dan.kjeldstrom.hansen <dan.kjeldstrom.hansen@qliro.com>
2024-01-04 13:13:35 +0100

    reformatting

--- day4/src/main/scala/Main.scala
+++ day4/src/main/scala/Main.scala
@@ -1,11 +1,14 @@
+import scala.collection.mutable
 import scala.util.Using
 
-lazy val wins: Seq[Int] = {
-    val src = Using(
-      io.Source.fromFile("/home/dankh/scalaprojects/aoc_2023/sources/4.txt")
-    )(_.getLines.toList)
+@main def run(): Unit =
+    println(day4a(wins)) // 27454
+    println(day4b(wins)) // 6857330
 
-    src.get
+private def wins: List[Int] = {
+    Using(
+      io.Source.fromFile("/home/dankh/scalaprojects/aoc_2023/sources/4.txt")
+    )(_.getLines.toList).get
         .map(l =>
             (
               l.substring(l.indexOf(":") + 1, l.indexOf("|"))
@@ -19,24 +22,19 @@ lazy val wins: Seq[Int] = {
             )
         )
         .map(t => t._2.intersect(t._1).length)
-
 }
 
-@main def run(): Unit =
-    println(day4a(wins)) // 27454
-    println(day4b(wins)) // 6857330
+private def day4a(input: List[Int]): Int =
+    input.map(x => if (x == 0) 0 else math.pow(2, x - 1).toInt).sum
 
-def day4a(input: Seq[Int]): Int =
-    input.map(x => if x == 0 then 0 else math.pow(2, x - 1).toInt).sum
-
-def day4b(input: Seq[Int]): Int =
-    val queue = collection.mutable.Queue(1)
-    // re-write to reduce instead of foldLeft
+private def day4b(input: List[Int]): Int =
+    val queue = mutable.Queue(1)
     input.foldLeft(0) { (idx, copies) =>
-        val count = if (queue.isEmpty) 1 else queue.dequeue
-        for (i <- 0 until copies) {
+        val count: Int =
+            if (queue.isEmpty) 1
+            else queue.dequeue
+        for (i <- 0 until copies)
             if (i < queue.size) queue.update(i, queue(i) + count)
-            else queue.append(count + 1)
-        }
+            else queue.enqueue(count + 1)
         idx + count
     }
