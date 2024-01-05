001d2a9104ed3595c6202d45b0692905cbdd93a9
dan.kjeldstrom.hansen <dan.kjeldstrom.hansen@qliro.com>
2024-01-05 11:38:38 +0100

    day5

--- day5/src/main/scala/main.scala
+++ day5/src/main/scala/main.scala
@@ -1,4 +1,14 @@
+import scala.util.{Failure, Success, Try}
+
 @main
 def main(): Unit = {
-  println("Hello world!")
+    val input: Seq[String] = almanac match
+        case Failure(ex)    => List(ex.getMessage)
+        case Success(value) => value
 }
+final def almanac: Try[List[String]] =
+    val file = "5a_ex.txt"
+    val mainFolder = "/home/dankh/scalaprojects/aoc_2023/sources/"
+    util.Using(io.Source.fromFile(mainFolder + file))(
+      _.mkString.split("\r?\n\\s*\r?\n").toList
+    )
